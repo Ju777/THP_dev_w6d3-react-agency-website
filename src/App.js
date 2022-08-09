9a7dee7e3ef23bf7547139c3b8a402b5b2d8228e
Julien Armagnac <julien.tamil@gmail.com>
2022-08-09 08:10:03 +0200

    Add Dark/light mode feature. Still need to store user choice in localStorage .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -8,20 +8,34 @@ import About from './pages/About';
 import Works from './pages/Works';
 import Work from './components/Work';
 
+import ThemeContext from './contexts/ThemeContext';
+
 function App() {
+
+  const [ currentTheme, setCurrentTheme ] = useState();
+
+  const themeContextValue = {
+    theme : currentTheme,
+    updateTheme : setCurrentTheme
+  }
+
+
   return (
-    <div className="">
-      <BrowserRouter>
-        <Routes>
-          <Route path = "/" element = {<SharedLayout/>}>
-            <Route index element = {<Home/>}/>
-            <Route path = "/about" element = {<About/>}/>
-            <Route path = '/works' element = {<Works/>}/>
-            <Route path = '/works/:workSlug' element = {<Work/>}/>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeContext.Provider value = {themeContextValue}>
+      <div className = {currentTheme} >
+        <BrowserRouter>
+          <Routes>
+            <Route path = "/" element = {<SharedLayout/>}>
+              <Route index element = {<Home/>}/>
+              <Route path = "/about" element = {<About/>}/>
+              <Route path = '/works' element = {<Works/>}/>
+              <Route path = '/works/:workSlug' element = {<Work/>}/>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
+
   );
 }
 
--- src/components/SharedLayout.js
+++ src/components/SharedLayout.js
@@ -1,15 +1,30 @@
 import '../styles/SharedLayout.css'
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import ThemeContext from '../contexts/ThemeContext';
 
 function SharedLayout(){
+
+    const [ currentTheme, setCurrentTheme ] = useState('light');
+    const { updateTheme } = useContext(ThemeContext);
+
+    function handleClick(e){
+        console.log(e.target.value);
+        updateTheme(e.target.value);
+        currentTheme === "light" ? setCurrentTheme("dark") : setCurrentTheme("light");
+    }
+
     return(
         <div id="navbar-page-container">
             <nav id="navbar">
                 <div><Link to ="/" >Home</Link></div>
                 <div><Link to ="/about" >About</Link></div>
                 <div><Link to ="/works" >Works</Link></div>
-                <button>Dark/Light</button>
+                <button
+                    value = {currentTheme}
+                    onClick = { (e) => handleClick(e) }
+                >{currentTheme === "light" ? "LIGHT MODE" : "DARK MODE" }
+                </button>
             </nav>
             <Outlet/>
         </div>
