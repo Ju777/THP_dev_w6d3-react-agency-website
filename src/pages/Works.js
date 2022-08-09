bcfb9a63152c92055d7cfea814cf021c190a64ed
Julien Armagnac <julien.tamil@gmail.com>
2022-08-09 11:04:44 +0200

    Update README.md

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Works(){
     return(
         <div className='page-container'>
-            {/* <h1>Current page : WORKS</h1> */}
+            <h1>PROJETS</h1>
             <h3>Au fil des années, nous avons pu accompagner les meilleurs.</h3>
             <p>Découvrez pas à pas comment nous avons été présents pour lancer vos marques préférées.</p>
             <div id="study-cases-container">
